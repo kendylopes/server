f56790be8cfb6c36c393e8761ba197b5c647b7f6
kendylopes <kendylopes@gmail.com>
2025-07-09 05:36:33 -0400

    feat:  correção de get-rooms

--- src/http/routes/get-rooms.ts
+++ src/http/routes/get-rooms.ts
@@ -1,4 +1,5 @@
-import type { FastifyPluginAsyncZod, FastifyPluginCallbackZod } from "fastify-type-provider-zod"
+
+import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod"
 import { schema } from "../../db/schema/index.ts"
 import { db } from "../../db/connections.ts"
 
