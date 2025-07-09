ed4550472763d2d6ec5881019ec4f204b7b1beee
kendylopes <kendylopes@gmail.com>
2025-07-08 22:52:33 -0400

    feat: add rooms listing

---
+++ src/http/routes/get-rooms.ts
@@ -0,0 +1,18 @@
+import type { FastifyPluginAsyncZod, FastifyPluginCallbackZod } from "fastify-type-provider-zod"
+import { schema } from "../../db/schema/index.ts"
+import { db } from "../../db/connections.ts"
+
+export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
+  app.get('/rooms', async () => {
+    const results = await db
+      .select({
+        id: schema.rooms.id,
+        name: schema.rooms.name
+      })
+      .from(schema.rooms)
+      .orderBy(schema.rooms.createdAt)
+
+    return results
+
+  })
+}
